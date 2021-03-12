ae261e4be5a266ceb9d75e41d39d06ce06e58537
Abhi Pujari <abhishekpujari.08@gmail.com>
2021-03-12 21:53:57 +0530

    Nav-Bar created with logo, search bar, options and basket

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAS3LLQ1ucMdrN9uzIan5pw8AD9O5EBtaY",
+  authDomain: "challenge-75df2.firebaseapp.com",
+  projectId: "challenge-75df2",
+  storageBucket: "challenge-75df2.appspot.com",
+  messagingSenderId: "479506191108",
+  appId: "1:479506191108:web:425ddc49d222cbffba3651",
+  measurementId: "G-GBT59HY4L5"
+};
